3ba159665c1732572618ccfebf4ea413a402e260
JeanCDev <jeangames15@gmail.com>
2020-11-18 15:04:14 -0300

    dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import routes from './routes';
+import dotenv from 'dotenv';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 
-app.listen(8080, ()=> {
+app.listen(process.env.PORT || 8080, ()=> {
   console.log('Server Started');
 });
